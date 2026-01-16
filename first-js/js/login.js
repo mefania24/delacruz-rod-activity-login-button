88da43c0af663dee2ef0bc23f89f87cf49885f53
mefania24 <lianmallaribrosa@gmail.com>
2026-01-15 21:26:11 -1000

    Add files via upload

---
+++ first-js/js/login.js
@@ -0,0 +1,17 @@
+function login() {
+//old ways
+var name = 'Spirus E Bersamina';
+var age = '22';
+
+//new way
+const num1 = 100;
+let num2 = 500;
+num2 = 300;
+
+const sum = num1 + num2;
+alert("the sum is " + sum)
+}
+
+function sample() {
+    alert("error")
+}
